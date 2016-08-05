fc0d9f2e2e75a9ea949e7c22e24cf0761e913f01
Daphne Solutions <yes@daphne-solutions.com>
2016-08-05 18:59:44 +0200

    first test cases

--- pyEMCA/tests/pyemca_test.py
+++ pyEMCA/tests/pyemca_test.py
@@ -5,7 +5,7 @@ Tests for the pyEMCA function.
 """
 
 import unittest
-from ..pyemca import Dat, pSupI, SupI
+from ..pyemca import Dat, pSupI, SupI, pSupIV, SupIV, bal, liv, serLin
 
 class TestEMCA(unittest.TestCase):
     """
@@ -59,6 +59,62 @@ class TestEMCA(unittest.TestCase):
         self.assertEquals(supB, -14040.436456996149)
         self.assertEquals(supC, -36505.134788189986)        
         
+    def test_psup4(self):
+        """
+        Test a simple, valid example.
+        """        
+        psupA = pSupIV(self.prza, self.pmededia, self.supesta, self.supa, self.supseca, self.indmerca)
+        psupB = pSupIV(self.przb, self.pmededib, self.supestb, self.supb, self.supsecb, self.indmercb)
+        psupC = pSupIV(self.przc, self.pmededic, self.supestc, self.supc, self.supsecc, self.indmercc)
+        psupmin = min(psupA, psupB, psupC)
+        supA = SupIV(psupmin, 105, 130)
+        supB = SupIV(psupmin, 105, 110)
+        supC = SupIV(psupmin, 105, 118)        
+        self.assertEquals(supA, -70172.09563543004)
+        self.assertEquals(supB, -14034.419127086006)
+        self.assertEquals(supC, -36489.48973042362)  
         
+    def test_bal1(self):
+        """
+        Test a simple, valid example.
+        """   
+        psupA=pSupI(self.prza, self.supa, self.supseca, self.indmerca)
+        psupB=pSupI(self.przb, self.supb, self.supsecb, self.indmercb)
+        psupC=pSupI(self.przc, self.supc, self.supsecc, self.indmercc)
+        psupmin = min(psupA, psupB, psupC)
+        bala=bal(psupmin, 0.33, 8, 8)
+        balb=bal(psupmin, 0.33, 8, 4)
+        balc=bal(psupmin, 0.33, 8, 8)
+        self.assertEquals(bala, 0.0)
+        self.assertEquals(balb, 3706.6752246469837)
+        self.assertEquals(balc, 0.0)           
+
+    def test_bal4(self):
+        """
+        Test a simple, valid example.
+        """   
+
+    def test_liv(self):
+        """
+        Test a simple, valid example.
+        """   
+        liva=liv(self.prza, 0.01, 3, 3)
+        livb=liv(self.przb, 0.01, 3, 2)
+        livc=liv(self.przc, 0.01, 3, 4)
+        self.assertEquals(liva, 0.0)
+        self.assertEquals(livb, 3150.0)
+        self.assertEquals(livc, -3465.3465346534654)   
+        
+    def test_serlin(self):
+        """
+        Test a simple, valid example.
+        """   
+        serA=serLin(15000, 20, 30, 1, 2)
+        serB=serLin(15000, 20, 30, 1, 1)
+        serC=serLin(15000, 20, 30, 1, 2)  
+        self.assertEquals(serA, -5000.000000000001)
+        self.assertEquals(serB, 0.0)
+        self.assertEquals(serC, -5000.000000000001)        
+       
 if __name__ == '__main__':
     unittest.main()
