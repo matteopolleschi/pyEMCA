05e25a299e77984cebd3d7333383285498caae48
Daphne Solutions <yes@daphne-solutions.com>
2016-08-05 15:51:55 +0200

    added test folder

--- pyEMCA/tests/pyemca_test.py
+++ pyEMCA/tests/pyemca_test.py
@@ -6,3 +6,18 @@ Tests for the pyEMCA function.
 
 import unittest
 from ..pyemca import Dat
+
+class TestEMCA(unittest.TestCase):
+    """
+    Test.
+    """
+
+    def test_basic(self):
+        """
+        Test a simple, valid example.
+        """
+        print "ciao"
+        self.assertEquals(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
