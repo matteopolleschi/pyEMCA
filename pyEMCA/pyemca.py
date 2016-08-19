2866ba5e69a6301f39c42d83c6814d611fc9896c
Daphne Solutions <yes@daphne-solutions.com>
2016-08-19 23:40:48 +0200

    refactoring

--- pyEMCA/pyemca.py
+++ pyEMCA/pyemca.py
@@ -12,21 +12,16 @@ def Dat(prz, saggio, mesidastima, mesiatto):
 #Primo teorema
 #supsec e indici sono due liste
 def pSupI(prz, supPrinc, supSec, indici):
-    i = 0
     supSecInd = 0
     psup = 0
 
     if not (isinstance(supSec, (int, long))):
-        for superficie in supSec:
-            supSecInd += superficie * indici[i]
-            #print "Debug"
-            #print superficie, indici[i]
-            i = i + 1
+        for (k,v),(k2,v2) in zip(supSec.items(),indici.items()):
+            supSecInd += v * v2
     else:
         supSecInd += supSec * indici
 
     psup = prz / (supPrinc + supSecInd)
-    #print psup
     return psup
 
 def SupI(psupmin,supSubject, stimaComp):
@@ -36,32 +31,24 @@ def SupI(psupmin,supSubject, stimaComp):
 
 #Quarto teorema
 #pMedEdi, supEstm, supSec, indici -> Liste
-def pSupIV(prz, pMedEdi, supEstm, supPrinc, supSec, indici):
-    i = 0
+def pSupIV(prz, pMed, supEstm, supPrinc, supSec, indici):
     pMedEst = 0
     supSecInd = 0
     psup=0
 
-    if not(isinstance(pMedEdi, (int, long))):
-        for pmed in pMedEdi:
-           pMedEst = pmed + supEstm[i]
-           i = i + 1
-        i = 0
-
+    if not(isinstance(supEstm, (int, long))):
+        for sup in supEstm:
+           pMedEst = pMed + sup
     else:
-        pMedEst = pMedEdi + supEstm
-    #print pMedEst
+        pMedEst = pMed + supEstm
 
     if not (isinstance(supSec, (int, long))):
-        for superficie in supSec:
-            supSecInd += superficie * indici[i]
-            i = i + 1
-        #print supSecInd
+        for (k,v),(k2,v2) in zip(supSec.items(),indici.items()):
+            supSecInd += v * v2
     else:
         supSecInd += supSec * indici
 
     psup = (prz - pMedEst) / (supPrinc + supSecInd)
-    #print psup
     return psup
 
 def SupIV(psupmin, supSubject, stimaComp):
@@ -119,7 +106,6 @@ def StmUEEC(costoNuovoSTM, vitautile, anzRistSoggStm, anzRistComp, annoComp, ann
     p2=pow(y2, 2)
 
     pstm = (((costoNuovoSTM *100)/140) * p1 - p2)/(year-annoComp)
-    #print pstm
     stm = pstm * (anzRistSoggStm - anzRistComp)
     return stm
     
@@ -133,14 +119,14 @@ def qualitative_correction(base_matrix, corrected_prices):
     result = np.dot(moore_penrose, corrected_prices)
     return result
     
-def estimation(subject, records, impianti, pdatprz, surface_sec_indexes, theorem, costo_impianti, qualitative, costo_qualitative):
+def estimation(subject, records, impianti, pDatPrz, pMed, surface_sec_indexes, theorem, costo_impianti, qualitative, costo_qualitative):
     for record in records:
-        record['dat'] = Dat(record['price'], pdatprz, subject['date'], record['date'])
+        record['dat'] = Dat(record['price'], pDatPrz, subject['date'], record['date'])
         if theorem==1: 
             record['psup'] = pSupI(record['price'], record['surface'], record['surface_sec'], surface_sec_indexes)
-#        elif theorem==4: 
-#            record['psup'] = pSupIV(record['price'], self.pmededia, self.supesta, record['surface'], record['surface_sec'], surface_sec_indexes)
-#        else: break      
+        elif theorem==4: 
+            record['psup'] = pSupIV(record['price'], pMed, supesta, record['surface'], record['surface_sec'], surface_sec_indexes)
+        else: break      
     psupmin = min([record['psup'] for record in records])
     for record in records:
         if theorem==1: 
--- pyEMCA/tests/pyemca_test.py
+++ pyEMCA/tests/pyemca_test.py
@@ -13,28 +13,28 @@ class TestEMCA(unittest.TestCase):
     """
     Test.
     """
-    supseca=[8, 10]
-    supsecb=[4, 0]
-    supsecc=[8, 8]
-    indmerca=[0.33, 0.5]
-    indmercb=[0.33, 0.5]
-    indmercc=[0.33, 0.5]
-    pmededia=[100]
-    pmededib=[100]
-    pmededic=[100]
+    supseca={'3':8, '23':10}
+    supsecb={'3':4, '23':0}
+    supsecc={'3':8, '23':8}
+    pDatPrz=0.02
+    pLivPrz=0.01
+    pMed=100
+    indmerc={'3':0.33, '23':0.5}
     supesta=[120]
     supestb=[0]
     supestc=[50]
-    subject = {'date': 0, 'surface': 105}
-    records = [{'price': 460000, 'date': 6, 'surface': 130, 'surface_sec': supseca},{'price': 315000, 'date': 1, 'surface': 110, 'surface_sec': supsecb},{'price': 350000, 'date':1, 'surface': 118, 'surface_sec': supsecc}]
+    subject = {'date': 0, 'surface': 105, 'floor': 3, 'year_works': 2010}
+    records = [{'price': 460000, 'date': 6, 'surface': 130, 'surface_sec': supseca, 'surface_sec_index': indmerc, 'floor': 3, 'year_works': 2000}, \
+    {'price': 315000, 'date': 1, 'surface': 110, 'surface_sec': supsecb, 'surface_sec_index': indmerc, 'floor': 2, 'year_works': 2001}, \
+    {'price': 350000, 'date':1, 'surface': 118, 'surface_sec': supsecc, 'surface_sec_index': indmerc, 'floor': 4, 'year_works': 2008}]
         
     def test_dat(self):
         """
         Test a simple, valid example.
         """
-        DATa=Dat(self.records[0]['price'], 0.02, 0, 6)
-        DATb=Dat(self.records[1]['price'], 0.02, 0, 1)
-        DATc=Dat(self.records[2]['price'], 0.02, 0, 1)
+        DATa=Dat(self.records[0]['price'], self.pDatPrz, self.subject['date'], self.records[0]['date'])
+        DATb=Dat(self.records[1]['price'], self.pDatPrz, self.subject['date'], self.records[1]['date'])
+        DATc=Dat(self.records[2]['price'], self.pDatPrz, self.subject['date'], self.records[2]['date'])
         self.assertEquals(DATa, 4600.0)
         self.assertEquals(DATb, 525.0)
         self.assertEquals(DATc, 583.3333333333334)
@@ -43,13 +43,13 @@ class TestEMCA(unittest.TestCase):
         """
         Test a simple, valid example.
         """
-        psupA=pSupI(self.records[0]['price'], self.records[0]['surface'], [8, 10], self.indmerca)
-        psupB=pSupI(self.records[1]['price'], self.records[1]['surface'], self.supsecb, self.indmercb)
-        psupC=pSupI(self.records[2]['price'], self.records[2]['surface'], self.supsecc, self.indmercc)
+        psupA=pSupI(self.records[0]['price'], self.records[0]['surface'], self.records[0]['surface_sec'], self.indmerc)
+        psupB=pSupI(self.records[1]['price'], self.records[1]['surface'], self.supsecb, self.indmerc)
+        psupC=pSupI(self.records[2]['price'], self.records[2]['surface'], self.supsecc, self.indmerc)
         psupmin = min(psupA, psupB, psupC)
-        supA = SupI(psupmin, 105, 130)
-        supB = SupI(psupmin, 105, 110)
-        supC = SupI(psupmin, 105, 118)
+        supA = SupI(psupmin, self.subject['surface'], self.records[0]['surface'])
+        supB = SupI(psupmin, self.subject['surface'], self.records[1]['surface'])
+        supC = SupI(psupmin, self.subject['surface'], self.records[2]['surface'])
         self.assertEquals(supA, -70202.18228498075)
         self.assertEquals(supB, -14040.436456996149)
         self.assertEquals(supC, -36505.134788189986)        
@@ -58,13 +58,13 @@ class TestEMCA(unittest.TestCase):
         """
         Test a simple, valid example.
         """        
-        psupA = pSupIV(self.records[0]['price'], self.pmededia, self.supesta, self.records[0]['surface'], self.supseca, self.indmerca)
-        psupB = pSupIV(self.records[1]['price'], self.pmededib, self.supestb, self.records[1]['surface'], self.supsecb, self.indmercb)
-        psupC = pSupIV(self.records[2]['price'], self.pmededic, self.supestc, self.records[2]['surface'], self.supsecc, self.indmercc)
+        psupA = pSupIV(self.records[0]['price'], self.pMed, self.supesta, self.records[0]['surface'], self.supseca, self.indmerc)
+        psupB = pSupIV(self.records[1]['price'], self.pMed, self.supestb, self.records[1]['surface'], self.supsecb, self.indmerc)
+        psupC = pSupIV(self.records[2]['price'], self.pMed, self.supestc, self.records[2]['surface'], self.supsecc, self.indmerc)
         psupmin = min(psupA, psupB, psupC)
-        supA = SupIV(psupmin, 105, 130)
-        supB = SupIV(psupmin, 105, 110)
-        supC = SupIV(psupmin, 105, 118)        
+        supA = SupIV(psupmin, self.subject['surface'], self.records[0]['surface'])
+        supB = SupIV(psupmin, self.subject['surface'], self.records[1]['surface'])
+        supC = SupIV(psupmin, self.subject['surface'], self.records[2]['surface'])        
         self.assertEquals(supA, -70172.09563543004)
         self.assertEquals(supB, -14034.419127086006)
         self.assertEquals(supC, -36489.48973042362)  
@@ -73,9 +73,9 @@ class TestEMCA(unittest.TestCase):
         """
         Test a simple, valid example.
         """   
-        psupA=pSupI(self.records[0]['price'], self.records[0]['surface'], self.supseca, self.indmerca)
-        psupB=pSupI(self.records[1]['price'], self.records[1]['surface'], self.supsecb, self.indmercb)
-        psupC=pSupI(self.records[2]['price'], self.records[2]['surface'], self.supsecc, self.indmercc)
+        psupA=pSupI(self.records[0]['price'], self.records[0]['surface'], self.supseca, self.indmerc)
+        psupB=pSupI(self.records[1]['price'], self.records[1]['surface'], self.supsecb, self.indmerc)
+        psupC=pSupI(self.records[2]['price'], self.records[2]['surface'], self.supsecc, self.indmerc)
         psupmin = min(psupA, psupB, psupC)
         bala=bal(psupmin, 0.33, 8, 8)
         balb=bal(psupmin, 0.33, 8, 4)
@@ -88,9 +88,9 @@ class TestEMCA(unittest.TestCase):
         """
         Test a simple, valid example.
         """  
-        psupA = pSupIV(self.records[0]['price'], self.pmededia, self.supesta, self.records[0]['surface'], self.supseca, self.indmerca)
-        psupB = pSupIV(self.records[1]['price'], self.pmededib, self.supestb, self.records[1]['surface'], self.supsecb, self.indmercb)
-        psupC = pSupIV(self.records[2]['price'], self.pmededic, self.supestc, self.records[2]['surface'], self.supsecc, self.indmercc)
+        psupA = pSupIV(self.records[0]['price'], self.pMed, self.supesta, self.records[0]['surface'], self.supseca, self.indmerc)
+        psupB = pSupIV(self.records[1]['price'], self.pMed, self.supestb, self.records[1]['surface'], self.supsecb, self.indmerc)
+        psupC = pSupIV(self.records[2]['price'], self.pMed, self.supestc, self.records[2]['surface'], self.supsecc, self.indmerc)
         psupmin = min(psupA, psupB, psupC)
         bala=bal(psupmin, 0.33, 8, 8)
         balb=bal(psupmin, 0.33, 8, 4)
@@ -103,9 +103,9 @@ class TestEMCA(unittest.TestCase):
         """
         Test a simple, valid example.
         """   
-        liva=liv(self.records[0]['price'], 0.01, 3, 3)
-        livb=liv(self.records[1]['price'], 0.01, 3, 2)
-        livc=liv(self.records[2]['price'], 0.01, 3, 4)
+        liva=liv(self.records[0]['price'], self.pLivPrz, self.subject['floor'], self.records[0]['floor'])
+        livb=liv(self.records[1]['price'], self.pLivPrz, self.subject['floor'], self.records[1]['floor'])
+        livc=liv(self.records[2]['price'], self.pLivPrz, self.subject['floor'], self.records[2]['floor'])
         self.assertEquals(liva, 0.0)
         self.assertEquals(livb, 3150.0)
         self.assertEquals(livc, -3465.3465346534654)   
@@ -147,9 +147,9 @@ class TestEMCA(unittest.TestCase):
         """
         Test a s
         """ 
-        stma=StmLin(50000,50, 2010, 2000)
-        stmb=StmLin(50000, 50, 2010, 2001)
-        stmc=StmLin(50000, 50, 2010, 2008)       
+        stma=StmLin(50000, 50, self.subject['year_works'], self.records[0]['year_works'])
+        stmb=StmLin(50000, 50, self.subject['year_works'], self.records[1]['year_works'])
+        stmc=StmLin(50000, 50, self.subject['year_works'], self.records[2]['year_works'])       
         self.assertEquals(stma, 10000.0)
         self.assertEquals(stmb, 9000.0)
         self.assertEquals(stmc, 2000.0)
@@ -176,7 +176,7 @@ class TestEMCA(unittest.TestCase):
         self.assertTrue(np.allclose(last_step, benchmark, rtol=1e-03, atol=1e-08, equal_nan=False))
         
     def test_estimation(self):
-        estimation(subject=self.subject, records=self.records, pdatprz=0.02, surface_sec_indexes=[0.33, 0.5], theorem=1, impianti=1, costo_impianti=1, qualitative=1, costo_qualitative=1)
+        estimation(subject=self.subject, records=self.records, pDatPrz=self.pDatPrz, pMed=self.pMed, surface_sec_indexes=self.indmerc, theorem=1, impianti=1, costo_impianti=1, qualitative=1, costo_qualitative=1)
         self.assertTrue(1)
        
 if __name__ == '__main__':
